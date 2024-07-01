933d8edb14389e5f39323792b4b0d9308b5e2562
Aleksandr <87357087+Aleksandr-Nov@users.noreply.github.com>
2024-07-01 11:50:59 +0300

    Задание 3

--- NavAuto/Schemas/NavSchema1b1ba8ceDetail/NavSchema1b1ba8ceDetail.js
+++ NavAuto/Schemas/NavSchema1b1ba8ceDetail/NavSchema1b1ba8ceDetail.js
@@ -2,7 +2,8 @@ define("NavSchema1b1ba8ceDetail", [], function() {
 	return {
 		entitySchemaName: "Invoice",
 		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		diff: /**SCHEMA_DIFF*/[
+		]/**SCHEMA_DIFF*/,
 		methods: {}
 	};
 });
